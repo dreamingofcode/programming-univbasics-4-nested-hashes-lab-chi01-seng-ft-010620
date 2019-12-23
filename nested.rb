2f79b080150e8e26665b59f9df6b1cd2db1a3b59
cesar mota <cesarmota@ymail.com>
2019-12-23 01:44:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matfsssssssssasasa }
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
 
 	programmer_hash = 
  		{
