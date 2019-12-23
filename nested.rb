f7032aa0901dc632da719f9ec05add5ab4b7c48e
cesar mota <cesarmota@ymail.com>
2019-12-23 01:39:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    p rogrammer_hash[:dennis_ritchie][:languages]
+    pprogrammer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
