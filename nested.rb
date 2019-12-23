a10f64464004534730b9040a1b59e7e9a298cdb6
cesar mota <cesarmota@ymail.com>
2019-12-23 02:12:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
      }
     
 matz= {:yukihiro_matsumoto=>{:known_for=>"Ruby",:languages=>["LISP","C"]}}
-programmer_hash.merge(matz)
+programmer_hash.merge(matz[:yukihiro_matsumoto])
 programmer_hash
     
 end
