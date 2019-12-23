a99a4ff069a6abe6b0f0a447de0f3cd311a1b957
cesar mota <cesarmota@ymail.com>
2019-12-23 02:31:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,6 +115,15 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
 end
 
+
+
+
+
+
+
+
+
+
 def adding_to_dennis
 	programmer_hash = 
  		{
@@ -131,5 +140,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+programmer_hash
 end
