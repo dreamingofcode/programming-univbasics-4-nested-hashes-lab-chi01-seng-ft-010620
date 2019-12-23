cc5ce604d0148d5c3759ad73cf2f12aaf6d9828f
cesar mota <cesarmota@ymail.com>
2019-12-23 01:37:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,9 +19,10 @@ def hopper
  programmer_hash[:grace_hopper]
 end
 
+
+
+
 def alan_kay_is_known_for
-
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -40,6 +41,9 @@ def alan_kay_is_known_for
      programmer_hash[:alan_kay][:known_for]
 end
 
+
+
+
 def dennis_ritchies_language
 	programmer_hash = 
  		{
@@ -56,6 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
