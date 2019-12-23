1cc4087d24b3884700f0440b080ef540e5774d7e
cesar mota <cesarmota@ymail.com>
2019-12-23 01:36:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-programmer_hash[:alan_kay][:known_for]
+
 
 	programmer_hash = 
  		{
@@ -36,7 +36,7 @@ programmer_hash[:alan_kay][:known_for]
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     }programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
