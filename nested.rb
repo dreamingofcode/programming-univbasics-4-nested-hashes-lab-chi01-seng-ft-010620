fc579be39043c684276838f45374924f455c3b41
cesar mota <cesarmota@ymail.com>
2019-12-23 01:49:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,11 +64,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  
-#  => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
@@ -87,6 +82,7 @@ def adding_matz
      }
 programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages]="LISP","C"
+programmer_hash
     
 end
 
